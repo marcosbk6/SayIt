84aa903ed347d9d58bb69095b27e112576afa600
marcosbk6 <marcos06roberto06@gmail.com>
2025-08-28 14:36:39 -0300

    feat(main): cards com animacao estilizados

---
+++ assests/js/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#texto-digitado', {
+    strings: ['<span class="say">Say</span><span class="It">It!</span>'],
+    typeSpeed: 50, 
+    backSpeed: 25,
+    backDelay: 1000,
+    loop: true
+})
