e1d41a14394be98fea799e27ef1325a7e23aaf12
marcosbk6 <marcos06roberto06@gmail.com>
2025-09-08 16:52:41 -0300

    style(ui): atualizado o layout e design

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,10 +1,4 @@
-var typed = new Typed('#texto-digitado', {
-    strings: ['<span class="say">Say</span><span class="It">It!</span>'],
-    typeSpeed: 50, 
-    backSpeed: 25,
-    backDelay: 1000,
-    loop: true
-});
+
 
 document.querySelectorAll('.cards').forEach((card, idx) => {
   const imagensOriginais = [
